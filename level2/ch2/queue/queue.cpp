816767eb48e0a2355d74790153681f6822874b2a
x0ym <x0ym@github.com>
2023-12-20 22:47:05 +0800

    23.12.20

--- level2/ch2/queue/queue.cpp
+++ level2/ch2/queue/queue.cpp
@@ -1,13 +1,10 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 /*
-
 hh 表示队头，tt 表示队尾
-
 hh <= tt 队列不为空
-
 */
 const int N = 1e5 + 10;
 int q[N], hh = 0, tt = -1;
@@ -15,13 +12,13 @@ int q[N], hh = 0, tt = -1;
 // 队尾入队
 void push_back(int x)
 {
-    q[++ tt] = x;
+    q[++tt] = x;
 }
 
 // 队头出队
 int pop_front()
 {
-    return q[hh ++];
+    return q[hh++];
 }
 
 // 取队首
